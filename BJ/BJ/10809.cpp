5221ae6c660980b92a25b4232691074823645112
mountain9975 <mountain9975@stu.gnu.ac.kr>
2018-06-29 16:55:30 +0900

    11654 solved

---
+++ BJ/BJ/10809.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int arr[26];
+char input[100];
+int main() {
+	memset(arr, -1, sizeof(arr));
+	cin >> input;
+	for(int s=0;input[s];s++){
+		if (arr[input[s] - 'a'] != -1)continue;
+		arr[input[s] - 'a'] = s;
+		
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i] << ' ';
+	}
+}
