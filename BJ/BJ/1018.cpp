02e8fc1b3860c0809c52f4e345e204667a7a4f84
mountain9975 <mountain9975@stu.gnu.ac.kr>
2018-07-26 17:29:54 +0900

    7/26

--- BJ/BJ/1018.cpp
+++ BJ/BJ/1018.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 char Board[51][51];
 int R, C;
-int cnt, min_cnt;
+int min_cnt = 1e9;
+int temp;
+int pt1_cnt, pt2_cnt;
+string pattern1[8] = { "WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW" };
+string pattern2[8] = { "BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB" };
 
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0);
@@ -15,47 +20,17 @@ int main() {
 
 	for (int i = 0; i <= R - 8; i++) {
 		for (int j = 0; j <= C - 8; j++) {
-			cnt = 0;
-			if (Board[i][j] == 'W') { //ÁÂ»ó´Ü Ã³À½ÀÌ W ÀÏ¶§
-				for (int m = 0; m < 8; m++) {
-					for (int n = 0; n < 8; n++) {
-						if (m % 2 == 0) {//Â¦¼ö Çà
-							if (n % 2 == 0 && Board[m + i][n + j] != 'W') //Â¦¼ö ¿­ && W ¾Æ´Ò ¶§
-								cnt++;
-							else if (n % 2 == 1 && Board[m + i][n + j] != 'B') //È¦¼ö ¿­ && B°¡ ¾Æ´Ò ‹š
-								cnt++;
-						}
-						else if (m % 2 == 1) { //È¦¼ö Çà
-							if (n % 2 == 0 && Board[m + i][n + j] != 'B') //Â¦¼ö¿­ && B°¡ ¾Æ´Ò ¶§
-								cnt++;
-							else if (n % 2 == 1 && Board[m + i][n + j] != 'W') //Â¦¼ö¿­ && W°¡ ¾Æ´Ò ‹š
-								cnt++;
-						}
-					}
+			for (int r = 0; r < 8; r++) {
+				for (int c = 0; c < 8; c++) {
+					if (Board[r + i][c + j] != pattern1[r][c]) pt1_cnt++;
+					else if (Board[r + i][c + j] != pattern2[r][c]) pt2_cnt++;
 				}
 			}
-			else if (Board[i][j] == 'B') { //ÁÂ»ó´Ü Ã³À½ÀÌ B ÀÏ¶§
-				for (int m = 0; m < 8; m++) {
-					for (int n = 0; n < 8; n++) {
-						if (m % 2 == 0) {//Â¦¼ö Çà
-							if (n % 2 == 0 && Board[m + i][n + j] != 'B') //Â¦¼ö ¿­ && B°¡ ¾Æ´Ò ¶§
-								cnt++;
-							else if (n % 2 == 1 && Board[m + i][n + j] != 'W') //È¦¼ö ¿­ && W°¡ ¾Æ´Ò ‹š
-								cnt++;
-						}
-						else if (m % 2 == 1) { //È¦¼ö Çà
-							if (n % 2 == 0 && Board[m + i][n + j] != 'W') //Â¦¼ö¿­ && W°¡ ¾Æ´Ò ¶§
-								cnt++;
-							else if (n % 2 == 1 && Board[m + i][n + j] != 'B') //Â¦¼ö¿­ && B°¡ ¾Æ´Ò ‹š
-								cnt++;
-						}
-					}
-				}
-			}
-			if (i == 0 && j == 0) min_cnt = cnt;
-			else {
-				if (cnt <= min_cnt) min_cnt = cnt;
-			}
+			if (pt1_cnt <= pt2_cnt) temp = pt1_cnt;
+			else temp = pt2_cnt;
+			if (temp < min_cnt) min_cnt = temp;
+			pt1_cnt = 0;
+			pt2_cnt = 0;
 		}
 	}
 	cout << min_cnt;
